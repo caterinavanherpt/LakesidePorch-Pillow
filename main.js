1627dbe1456461f63c90be44155b6e24f0ad0fc4
Caterina van Herpt <caterina.vanherpt@gmail.com>
2021-02-21 16:00:05 -0500

    Updated package json

--- main.js
+++ main.js
@@ -3,18 +3,14 @@
 $(function () {
 				var $hamburger = $(".hamburger");
 				$hamburger.on("click", function (e) {
-					$hamburger.toggleClass("is-active");
-					$(".nav__list").toggleClass("hide");
-					$(".nav__list").toggleClass("show");
-					$(".nav").toggleClass("nav__show");
+								$hamburger.toggleClass("is-active");
+								$(".nav__list").toggleClass("hide");
+								$(".nav__list").toggleClass("show");
+								$(".nav").toggleClass("nav__show");
 				});
 
 				$('.gallery').flickity({
-					// lazyLoad: true,
-					lazyLoad: 2,
-					draggable: '>1',
-					wrapAround: true,
-					adaptiveHeight: true,
-					fullscreen: true
+								setgallerySize: false,
+								lazyload: 2
 				});
 });
