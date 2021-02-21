2e7d254406784007250f1890d77fa563174ad355
Caterina van Herpt <caterina.vanherpt@gmail.com>
2021-02-21 15:00:25 -0500

    updating room photos & flickity

--- main.js
+++ main.js
@@ -3,14 +3,18 @@
 $(function () {
 				var $hamburger = $(".hamburger");
 				$hamburger.on("click", function (e) {
-								$hamburger.toggleClass("is-active");
-								$(".nav__list").toggleClass("hide");
-								$(".nav__list").toggleClass("show");
-								$(".nav").toggleClass("nav__show");
+					$hamburger.toggleClass("is-active");
+					$(".nav__list").toggleClass("hide");
+					$(".nav__list").toggleClass("show");
+					$(".nav").toggleClass("nav__show");
 				});
 
 				$('.gallery').flickity({
-								setgallerySize: false,
-								lazyload: 2
+					// lazyLoad: true,
+					lazyLoad: 2,
+					draggable: '>1',
+					wrapAround: true,
+					adaptiveHeight: true,
+					fullscreen: true
 				});
 });
